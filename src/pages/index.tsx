fc22cd90d1ed0bd28da806c319269b50a6e729b1
Joe Kennaugh <joebkennaugh@gmail.com>
2023-04-22 00:32:21 +0100

    Adding starter files

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>CarbonCode NextJS Tech Test</title>
+      </Head>
+      <main>
+        <h1 className='text-6xl text-black font-semibold'>
+          CarbonCode NextJS Tech Test
+        </h1>
+      </main>
+    </>
+  );
+}
